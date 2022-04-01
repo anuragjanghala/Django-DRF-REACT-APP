4db330faf194173d460c28336e87de8cfc3bfccd
anuragjanghala <42205669+anuragjanghala@users.noreply.github.com>
2022-04-01 12:49:15 +0100

    worked on permissions

--- django_backend/core/settings.py
+++ django_backend/core/settings.py
@@ -130,9 +130,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ]
 }
+
+# Permissions
+# AllowAny
+# IsAuthenticated
+# IsAdmin
+# IsAuthenticatedOrReadOnly
 
 
 
