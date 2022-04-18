1dbfcc96f01fec9694fd9e8a719c36c22828c0b8
anuragjanghala <42205669+anuragjanghala@users.noreply.github.com>
2022-04-18 20:35:44 +0100

    NEW:: added coreapi(supported by Django RF) for API docs creation

--- django_backend/core/settings.py
+++ django_backend/core/settings.py
@@ -138,7 +138,9 @@ REST_FRAMEWORK = {
     
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
 }
 
 # Permissions
--- django_backend/core/urls.py
+++ django_backend/core/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -14,6 +15,7 @@ urlpatterns = [
     path('api/', include('blog_api.urls', namespace='blog_api')),
     path('api/user/', include('users.urls', namespace='users')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('docs/', include_docs_urls(title="BlogAPI")),
     path('schema', get_schema_view(
         title="BlogAPI",
         description="API for BlogAPI",
